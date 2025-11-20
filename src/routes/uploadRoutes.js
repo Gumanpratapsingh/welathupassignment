2ba0f9d19b7b2e0fab9f8b7785b9c11c0885ce64
Guman Pratap Singh <gumanpratapsinghparmar@gmail.com>
2025-11-20 20:38:14 +0530

    async added

--- src/routes/uploadRoutes.js
+++ src/routes/uploadRoutes.js
@@ -40,7 +40,7 @@ router.post('/', (req, res) => {
 
     const extension = filename.includes('.') ? filename.split('.').pop().toLowerCase() : '';
 
-    const safe = isTextLikeFile({
+    const safe = await isTextLikeFile({
       mimeType: mimetype,
       extension,
       filename
